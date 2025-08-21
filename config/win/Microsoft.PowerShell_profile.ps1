1c32cf26cad31e6d798be7bea97d4d52468a540e
@Michelle CC <59771891+eslco@users.noreply.github.com>
2025-08-21 17:31:46 +0800

    Updated Microsoft.PowerShell_profile.ps1

--- config/win/Microsoft.PowerShell_profile.ps1
+++ config/win/Microsoft.PowerShell_profile.ps1
@@ -98,3 +98,13 @@ function mkdir ( $file_path) {
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
